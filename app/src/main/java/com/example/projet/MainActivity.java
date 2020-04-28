a3d55d323b623e503b379c6d3de23538492d381f
Cloctime <herbreteauguilaume56@gmail.com>
2020-04-28 14:09:35 +0200

    bouton navig

--- app/src/main/java/com/example/projet/MainActivity.java
+++ app/src/main/java/com/example/projet/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonNiveau;
+    private Button buttonComparaison;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Log.d("TAG", "salut la fouere ");
+        buttonNiveau=(Button)findViewById(R.id.buttonNiveau);
+        buttonNiveau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNiveau();
+            }
+        });
+        buttonComparaison=(Button)findViewById(R.id.buttonComparaison);
+        buttonComparaison.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openComparaison();
+            }
+        });
     }
+    public void openNiveau() {
+        Intent intent = new Intent(this, niveau.class);
+        startActivity(intent);
+    }
+    public void openComparaison() {
+        Intent intent = new Intent(this, comparaison.class);
+        startActivity(intent);
+    }
+
 }
